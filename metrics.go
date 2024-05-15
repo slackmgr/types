2ead21ff50eb85ab27f9e2c3c7fea2995a708934
Peter Aglen <peteraglen@gmail.com>
2024-05-15 20:42:17 +0200

    wip

--- metrics.go
+++ metrics.go
@@ -5,5 +5,5 @@ import "time"
 type Metrics interface {
 	RegisterCounter(name, help string, labels ...string)
 	AddToCounter(name string, value float64, labelValues ...string)
-	AddHttpRequestMetric(path, method string, statusCode int, duration time.Duration)
+	AddHTTPRequestMetric(path, method string, statusCode int, duration time.Duration)
 }
