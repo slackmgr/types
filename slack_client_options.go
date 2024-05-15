92e750ea2d363a3d2a02435f33efcf4060d9dc57
Peter Aglen <peteraglen@gmail.com>
2024-05-15 20:48:15 +0200

    wip

--- slack_client_options.go
+++ slack_client_options.go
@@ -2,50 +2,92 @@ package common
 
 import "time"
 
-type SlackOptions struct {
-	AppToken     string
-	BotToken     string
-	DebugLogging bool
-	DryRun       bool
-	Concurrency  int
+type SlackClientOption func(*SlackClientOptions)
 
+type SlackClientOptions struct {
+	DebugLogging                 bool
+	DryRun                       bool
+	Concurrency                  int
 	MaxAttemtsForRateLimitError  int
 	MaxAttemptsForTransientError int
 	MaxAttemptsForFatalError     int
-
-	MaxRateLimitErrorWaitTime time.Duration
-	MaxTransientErrorWaitTime time.Duration
-	MaxFatalErrorWaitTime     time.Duration
-
-	HTTPTimeout time.Duration
+	MaxRateLimitErrorWaitTime    time.Duration
+	MaxTransientErrorWaitTime    time.Duration
+	MaxFatalErrorWaitTime        time.Duration
+	HTTPTimeout                  time.Duration
 }
 
-func (o *SlackOptions) SetDefaults() {
-	if o.MaxAttemtsForRateLimitError <= 0 {
-		o.MaxAttemtsForRateLimitError = 10
+func NewSlackOptions() *SlackClientOptions {
+	return &SlackClientOptions{
+		DebugLogging:                 false,
+		DryRun:                       false,
+		Concurrency:                  10,
+		MaxAttemtsForRateLimitError:  10,
+		MaxAttemptsForTransientError: 5,
+		MaxAttemptsForFatalError:     5,
+		MaxRateLimitErrorWaitTime:    2 * time.Minute,
+		MaxTransientErrorWaitTime:    30 * time.Second,
+		MaxFatalErrorWaitTime:        30 * time.Second,
+		HTTPTimeout:                  30 * time.Second,
 	}
+}
 
-	if o.MaxAttemptsForTransientError <= 0 {
-		o.MaxAttemptsForTransientError = 5
+func WithDebugLogging(debug bool) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.DebugLogging = debug
 	}
+}
 
-	if o.MaxAttemptsForFatalError <= 0 {
-		o.MaxAttemptsForFatalError = 5
+func WithDryRun(dryRun bool) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.DryRun = dryRun
 	}
+}
 
-	if o.MaxRateLimitErrorWaitTime <= 0 {
-		o.MaxRateLimitErrorWaitTime = 2 * time.Minute
+func WithConcurrency(concurrency int) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.Concurrency = concurrency
 	}
+}
 
-	if o.MaxTransientErrorWaitTime <= 0 {
-		o.MaxTransientErrorWaitTime = 30 * time.Second
+func WithMaxAttemptsForRateLimitError(maxAttempts int) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.MaxAttemtsForRateLimitError = maxAttempts
 	}
+}
 
-	if o.MaxFatalErrorWaitTime <= 0 {
-		o.MaxFatalErrorWaitTime = 30 * time.Second
+func WithMaxAttemptsForTransientError(maxAttempts int) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.MaxAttemptsForTransientError = maxAttempts
 	}
+}
 
-	if o.HTTPTimeout <= 0 {
-		o.HTTPTimeout = 30 * time.Second
+func WithMaxAttemptsForFatalError(maxAttempts int) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.MaxAttemptsForFatalError = maxAttempts
+	}
+}
+
+func WithMaxRateLimitErrorWaitTime(waitTime time.Duration) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.MaxRateLimitErrorWaitTime = waitTime
+	}
+}
+
+func WithMaxTransientErrorWaitTime(waitTime time.Duration) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.MaxTransientErrorWaitTime = waitTime
+	}
+}
+
+func WithMaxFatalErrorWaitTime(waitTime time.Duration) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.MaxFatalErrorWaitTime = waitTime
+	}
+}
+
+func WithHTTPTimeout(timeout time.Duration) SlackClientOption {
+	return func(o *SlackClientOptions) {
+		o.HTTPTimeout = timeout
 	}
 }
